d2ae1fd9247ed24c3efcdee865d6115908efa2b9
Ranz <sarcosredilbertranz@gmail.com>
2023-07-31 23:03:27 +0800

    fix(invitationdetails): date update

--- src/components/Content/InvitationDetails.vue
+++ src/components/Content/InvitationDetails.vue
@@ -21,7 +21,7 @@
           <p>7 PM - 11 PM</p>
         </div>
       </div>
-      <p class="rsvp-text">PLEASE SEND RSVP BY OWL BEFORE JULY 30TH</p>
+      <p class="rsvp-text">PLEASE SEND RSVP BY OWL BEFORE AUG 14TH</p>
       <div class="divider"></div>
       <div class="row">
         <div class="col">
